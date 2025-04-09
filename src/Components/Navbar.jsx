ed6aead0ba32f4cdfa9556adfff79ee4fa863adb
saurabh-gc <saurabhchauhangb@gmail.com>
2025-04-09 13:14:45 +0530

    Update Navbar.jsx
    change bg color to pink

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -4,7 +4,7 @@ import { NavLink } from 'react-router-dom'
 const Navbar = () => {
   return (
     <div>
-        <div style={{backgroundColor:'black',padding:'10px'}}>
+        <div style={{backgroundColor:'pink',padding:'10px'}}>
             <NavLink to='/register' style={{color:'white',marginRight:'5px'}}>Register</NavLink>
             <NavLink to='/admin' style={{color:'white',marginRight:'5px'}}>Admin</NavLink>
         </div>
